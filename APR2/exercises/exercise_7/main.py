06fc62241e8304480540f119d9209c524117fa63
Ondřej Švorc <ondrejsvorc@email.cz>
2024-04-21 10:45:14 +0200

    Add seventh exercise from APR2 (tree representation)

---
+++ APR2/exercises/exercise_7/main.py
@@ -0,0 +1,11 @@
+# Strom - druh grafu
+# Graf je struktura, která obsahuje objekty, které jsou spolu provázané
+# Binary Search Tree = bude u státnic
+
+# Subtree = část
+# Siblings
+# Children
+# Parents
+# Edge
+# Listy = Leaf Nodes (poslední objekty)
+# Uzel = node (data, )
---
+++ APR2/exercises/exercise_7/node.py
@@ -0,0 +1,71 @@
+from typing import Any
+from uuid import uuid4
+
+
+class Node:
+    def __init__(self: "Node", data: Any):
+        self.id = uuid4()
+        self.data = data
+        self._children = []
+        self._parent = None
+
+    def add_child(self: "Node", child: "Node") -> None:
+        child._parent = self
+        self._children.append(child)
+
+    def get_height(self) -> int:
+        if not self._children:
+            return 0
+        return 1 + max(child.get_height() for child in self._children)
+
+    def count_leaves(self) -> int:
+        if not self._children:
+            return 1
+        return sum(child.count_leaves() for child in self._children)
+
+    def get_level(self: "Node") -> int:
+        level = 0
+        parent = self._parent
+        while parent:
+            level += 1
+            parent = parent._parent
+        return level
+
+    def __str__(self):
+        spaces = " " * self.get_level() * 3
+        prefix = spaces + "|__" if self._parent else ""
+        result = prefix + str(self.data) + "\n"
+        if self._children:
+            for child in self._children:
+                result += child.__str__()
+        return result
+
+
+def build_product_tree() -> None:
+    root = Node("Electronics")
+
+    laptop = Node("Laptop")
+    laptop.add_child(Node("Mac"))
+    laptop.add_child(Node("Surface"))
+    laptop.add_child(Node("Thinkpad"))
+
+    cellphone = Node("Cell Phone")
+    cellphone.add_child(Node("iPhone"))
+    cellphone.add_child(Node("Google Pixel"))
+    cellphone.add_child(Node("Vivo"))
+
+    tv = Node("TV")
+    tv.add_child(Node("Samsung"))
+    tv.add_child(Node("LG"))
+
+    root.add_child(laptop)
+    root.add_child(cellphone)
+    root.add_child(tv)
+
+    print(root)
+    print("Height of the tree:", root.get_height())
+    print("Number of leaves in the tree:", root.count_leaves())
+
+
+if __name__ == "__main__":
+    build_product_tree()
