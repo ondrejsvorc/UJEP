2a76c4ab15c78e021a145b47f68c16be62f5a85a
Ondřej Švorc <ondrejsvorc@email.cz>
2024-06-11 14:50:54 +0200

    Add script to drop database

---
+++ URDB/sql/MSSQL/drop.sql
@@ -0,0 +1,11 @@
+DROP TABLE [Restaurace].[RestauracePivonka].[PolozkyMenu];
+DROP TABLE [Restaurace].[RestauracePivonka].[PolozkyObjednavek];
+DROP TABLE [Restaurace].[RestauracePivonka].[Objednavky];
+DROP TABLE [Restaurace].[RestauracePivonka].[Menu];
+DROP TABLE [Restaurace].[RestauracePivonka].[Stoly];
+DROP TABLE [Restaurace].[RestauracePivonka].[Zamestnanci];
+DROP TABLE [Restaurace].[RestauracePivonka].[Pozice];
+DROP TABLE [Restaurace].[RestauracePivonka].[Zakaznici];
+
+DROP SCHEMA [Restaurace].[RestauracePivonka];
+DROP DATABASE [Restaurace];
