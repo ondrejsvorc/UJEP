981c6226858e7c302c1d0cce7d73df7e7d6339e1
Ondřej Švorc <ondrejsvorc@email.cz>
2024-10-29 09:14:14 +0100

    Add Factory design pattern implementation (OONV)

---
+++ OONV/exercises/Factory/Program.cs
@@ -0,0 +1,74 @@
+﻿class Program
+{
+    static void Main()
+    {
+      IDialog dialog = new WindowsDialog();
+      dialog.CreateButton();
+      dialog.Render();
+    }
+}
+
+public interface IButton
+{
+  public void Render();
+  public void OnClick();
+}
+
+public interface IDialog
+{
+  public void Render();
+  public IButton CreateButton();
+}
+public class WindowsDialog : IDialog
+{
+  public void Render()
+  {
+    IButton button = CreateButton();
+    button.Render();
+    Console.WriteLine("Rendering Windows dialog...");
+  }
+
+  public IButton CreateButton()
+  {
+    return new WindowsButton();
+  }
+}
+public class WebDialog : IDialog
+{
+  public void Render()
+  {
+    IButton button = CreateButton();
+    button.Render();
+    Console.WriteLine("Rendering web dialog...");
+  }
+
+  public IButton CreateButton()
+  {
+    return new HtmlButton();
+  }
+}
+
+public class WindowsButton : IButton
+{
+    public void OnClick()
+    {
+      Console.WriteLine("Clicked Windows button...");
+    }
+
+    public void Render()
+    {
+      Console.WriteLine("Rendering Windows button...");
+    }
+}
+public class HtmlButton : IButton
+{
+    public void OnClick()
+    {
+      Console.WriteLine("Clicked HTML button...");
+    }
+
+    public void Render()
+    {
+      Console.WriteLine("Rendering HTML button...");
+    }
+}
