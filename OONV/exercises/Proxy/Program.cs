ef1fe280c2ad615cc703139a5c466c3415bb74f1
Ondřej Švorc <ondrejsvorc@email.cz>
2024-11-03 17:12:49 +0100

    Add Proxy design pattern implementation (OONV)

---
+++ OONV/exercises/Proxy/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    static void Main()
+    {
+      Proxy proxy = new(new Service());
+      proxy.Operate();
+    }
+}
+
+interface IService
+{
+  void Operate();
+}
+class Service : IService
+{
+  public void Operate()
+  {
+    Console.WriteLine("Operating...");
+  }
+}
+
+class Proxy : IService
+{
+  private readonly IService _service;
+
+  public Proxy(IService service)
+  {
+    _service = service;
+  }
+
+  public bool CheckAccess() 
+  {
+    return true; 
+  }
+
+  public void Operate() 
+  {
+    if (!CheckAccess())
+    {
+      return;
+    }
+
+    _service.Operate();
+  }
+}
