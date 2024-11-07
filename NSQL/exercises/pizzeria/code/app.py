c038be87d7d0052fd0feabe382af9c3c07b82319
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2024-11-07 14:45:25 +0100

    Add Redis Queue with MongoDB (NSQL)

--- NSQL/exercises/pizzeria/code/app.py
+++ NSQL/exercises/pizzeria/code/app.py
@@ -1,20 +1,26 @@
 import json
 from flask import Flask, redirect, render_template, request, make_response
-from redis import Redis
+from redis import Connection, Redis
 from pymongo import MongoClient
 from helpers import add_pizza, delete_pizza, get_pizza, get_pizzas, update_pizza
+from rq import Queue, Worker
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 redis = Redis(host="redis", port=6379)
+redis_queue = Queue(connection=redis)
 mongo = MongoClient(
     host="mongodb", port=27017, username="admin", password="admin", authSource="admin"
 )
 db = mongo["somedb"]
+pizzas_collection = db["pizzas"]
 storage = db["storage"]
 sensor_1 = {"id": "0001", "name": "Temp"}
 storage.insert_one(sensor_1)
 
 
+# 1. Cache (Redis a MongoDB)
 @app.route("/sensor/<sensor_id>", methods=["GET"])
 def get_sensor(sensor_id):
     sensor_data = redis.get(f"sensor:{sensor_id}")
@@ -30,11 +36,27 @@ def get_sensor(sensor_id):
     return f"Data senzoru s id {sensor_id} nebyla nalezena", 404
 
 
+# 2. Counter
 @app.route("/", methods=["GET"])
 def home():
+    # redis.delete("homepage_requests")
     redis.incr("homepage_requests")
     counter = str(redis.get("homepage_requests"), "utf-8")
     return render_template("home.html", test=counter)
+
+
+# 3. Cache webové stránky
+@app.route("/page")
+def get_page():
+    # redis.delete("cached_page")
+    cached_page = redis.get("cached_page")
+    if cached_page:
+        logging.info("Načtení stránky z redisu.")
+        return cached_page.decode("utf-8"), 200
+    logging.info("Prvotní načtení.")
+    page = render_template("page.html")
+    redis.set("cached_page", page)
+    return page, 200
 
 
 @app.route("/pizzas", methods=["GET"])
@@ -68,13 +90,21 @@ def add_pizza_get():
     return render_template("add-pizza.html")
 
 
+## 4. Redis queue
+def add_pizza_to_mongo(pizza):
+    logging.info("Přidávám pizzu do MongoDB...")
+    pizzas_collection.insert_one(pizza)
+    print(f"Inserted pizza: {pizza}")
+
+
 @app.route("/add-pizza", methods=["POST"])
 def add_pizza_post():
     name = request.form.get("pizzaName")
     price = int(request.form.get("pizzaPrice"))
 
     pizza = {"name": name, "price": price}
-    add_pizza(pizza)
+    logging.info("Přidávám pizzu do Redis Queue...")
+    redis_queue.enqueue(add_pizza_to_mongo, pizza)
 
     return redirect("/pizzas")
 
@@ -138,3 +168,5 @@ def api_delete_pizza():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+    w = Worker(["default"], connection=Redis(host="redis", port=6379))
+    w.work()
