d010ea20d4fef854c0e37f8788837467ae0dd00a
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2024-11-26 16:47:48 +0100

    Improve LOCK example (RDBS)

--- RDBS/zapocet/sql/lock.sql
+++ RDBS/zapocet/sql/lock.sql
@@ -1,21 +1,29 @@
 -- Uzamknutí celé tabulky
-DROP TABLE IF EXISTS stoly_locked;
-CREATE TEMP TABLE stoly_locked AS (SELECT * FROM Pivonka.Stoly);
+DROP TABLE IF EXISTS Pivonka.stoly_locked;
+CREATE TABLE Pivonka.stoly_locked AS (SELECT * FROM Pivonka.Stoly);
 
 BEGIN;
-LOCK TABLE Pivonka.Stoly IN ACCESS EXCLUSIVE MODE;
-UPDATE stoly_locked SET PocetMist = PocetMist + 1;
+	LOCK TABLE Pivonka.stoly_locked IN ACCESS EXCLUSIVE MODE;
+	UPDATE Pivonka.stoly_locked SET PocetMist = PocetMist + 1;
+	SELECT pg_sleep(10); -- Simulace operace, která trvá 10 sekund
 COMMIT;
 
-SELECT * FROM stoly_locked;
+SELECT * FROM Pivonka.stoly_locked;
 
 -- Uzamknutí konkrétních řádků tabulky
-DROP TABLE IF EXISTS stoly_locked;
-CREATE TEMP TABLE stoly_locked AS (SELECT * FROM Pivonka.Stoly);
+-- Alternativa SELECT FOR UPDATE
+DROP TABLE IF EXISTS Pivonka.stoly_locked;
+CREATE TABLE Pivonka.stoly_locked AS (SELECT * FROM Pivonka.Stoly);
 
 BEGIN;
-SELECT * FROM Pivonka.Stoly WHERE PocetMist > 0 FOR UPDATE;
-UPDATE stoly_locked SET PocetMist = PocetMist + 1;
+	SELECT * FROM Pivonka.stoly_locked WHERE PocetMist > 0 FOR UPDATE;
+	UPDATE Pivonka.stoly_locked SET PocetMist = PocetMist + 1;
 COMMIT;
 
-SELECT * FROM stoly_locked;
+SELECT * FROM Pivonka.stoly_locked;
+
+-- Návod
+-- 1. Přidělit uživateli petr oprávnění k tabulce stoly_locked
+-- 2. Spustit uzamčení tabulky
+-- 3. Spustit SELECT * FROM Pivonka.stoly_locked; (petr_select_lock.sql) s uživatelem petr
+-- 4. Petrův příkaz by měl čekat na znovuodemčení tabulky
---
+++ RDBS/zapocet/sql/petr_select_lock.sql
@@ -0,0 +1,1 @@
+SELECT * FROM Pivonka.stoly_locked;
