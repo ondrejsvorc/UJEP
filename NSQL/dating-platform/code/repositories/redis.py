e1a3099f725ff11f4653dc99c92f0a4fd6eaad87
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2024-12-15 14:41:27 +0100

    Add get/set/delete methods to Redis, decode responses by default (Dating platform)

--- NSQL/dating-platform/code/repositories/redis.py
+++ NSQL/dating-platform/code/repositories/redis.py
@@ -1,6 +1,12 @@
+from enum import Enum
 import os
 from redis import Redis
 from repositories.repository import Repository
+
+
+class RedisKey(Enum):
+    MATCHES = "matches"
+    AVAILABLE_MATCHES = "available_matches"
 
 
 class RedisRepository(Repository):
@@ -15,9 +21,22 @@ class RedisRepository(Repository):
         self._setup_database()
         return self
 
+    def generate_key(self, key: RedisKey, identifier: str) -> str:
+        return f"{key.value}:{identifier}"
+
+    def delete(self, key: str):
+        self._db.delete(key)
+
+    def get(self, key: str):
+        return self._db.get(key)
+
+    def set(self, key: str, value, ex=3600):
+        self._db.set(key, value, ex)
+
     def _setup_database(self):
         self._db = Redis(
             host=os.getenv("REDIS_HOST"),
             port=int(os.getenv("REDIS_PORT")),
             password=os.getenv("REDIS_PASSWORD"),
+            decode_responses=True,
         )
