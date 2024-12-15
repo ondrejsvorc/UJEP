9eac2f6d3106c3ec6247733eb832e355d7f6b3ef
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2024-12-15 15:59:09 +0100

    Add missing disconnect methods (Dating platform)

--- NSQL/dating-platform/code/relationships.py
+++ NSQL/dating-platform/code/relationships.py
@@ -7,20 +7,33 @@ class RelationshipStrategy(ABC):
     def connect(self, user: Person, other: Person):
         pass
 
+    @abstractmethod
+    def disconnect(self, user: Person, other: Person):
+        pass
+
 
 class LikeStrategy(RelationshipStrategy):
     def connect(self, user: Person, other: Person):
         user.likes.connect(other)
+
+    def disconnect(self, user: Person, other: Person):
+        user.likes.disconnect(other)
 
 
 class DislikeStrategy(RelationshipStrategy):
     def connect(self, user: Person, other: Person):
         user.dislikes.connect(other)
 
+    def disconnect(self, user: Person, other: Person):
+        user.dislikes.disconnect(other)
+
 
 class SleepsWithStrategy(RelationshipStrategy):
     def connect(self, user: Person, other: Person):
         user.sleepsWith.connect(other)
+
+    def disconnect(self, user: Person, other: Person):
+        user.sleepsWith.disconnect(other)
 
 
 class RelationshipContext:
@@ -29,3 +42,6 @@ class RelationshipContext:
 
     def connect_users(self, user: Person, other: Person):
         return self._strategy.connect(user, other)
+
+    def disconnect_users(self, user: Person, other: Person):
+        return self._strategy.disconnect(user, other)
