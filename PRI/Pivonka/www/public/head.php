da999481f425a94dd71e35e2b386fe7beaaea774
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2025-03-21 11:13:49 +0100

    Add favicon (PRI)

--- PRI/Pivonka/www/public/head.php
+++ PRI/Pivonka/www/public/head.php
@@ -3,4 +3,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurace Pivoňka</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="icon.png">
 </head>
