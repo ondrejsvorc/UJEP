c96ac80a4ed1b069bf200b139a78bfd4b578593b
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2025-03-23 17:03:09 +0100

    Improve OperationResult to mimic generics behaviour even though PHP doesn't support them natively (PRI)

--- PRI/Pivonka/www/app/OperationResult.php
+++ PRI/Pivonka/www/app/OperationResult.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 final class OperationResult {
     public readonly bool $isSuccess;
     public readonly ?string $errorMessage;
+    public readonly ?mixed $data;
 
-    private function __construct(bool $isSuccess, ?string $errorMessage) {
+    private function __construct(bool $isSuccess, ?string $errorMessage, ?mixed $data) {
         $this->isSuccess = $isSuccess;
         $this->errorMessage = $errorMessage;
+        $this->data = $data;
     }
 
-    public static function success(): self {
-        return new self(true, null);
+    public static function success(?mixed $data = null): self {
+        return new self(true, null, $data);
     }
 
-    public static function failure(string $errorMessage): self {
-        return new self(false, $errorMessage);
+    public static function failure(string $errorMessage, ?mixed $data = null): self {
+        return new self(false, $errorMessage, $data);
     }
 }
