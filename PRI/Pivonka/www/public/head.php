03bc9ecad2b861db767e27825f450bb6a838ff68
Ondřej Švorc <52735242+ondrejsvorc@users.noreply.github.com>
2025-03-28 21:25:54 +0100

    Remove Pivonka project as it was replaced by BookTracker, remove obsolete docker-files (PRI)

--- PRI/Pivonka/www/public/head.php
+++
@@ -1,7 +0,0 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restaurace Pivoňka</title>
-    <link rel="stylesheet" href="styles.css">
-    <link rel="icon" href="icon.png">
-</head>
